2f08e2566352cd62fd2beeb477f4743f71d2f002
mnr-18 <75406127+mnr-18@users.noreply.github.com>
2021-01-27 10:26:03 -0700

    Contracts

---
+++ Contracts/uDir.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.15;
+
+contract uDir{
+    address public AuthServ;
+    address public Adj;
+    
+    struct UserData{
+        string pid; //pseudonym of user
+        string pk; //user public key (256 bytes)
+        string cert_pk; //user certificate on PK given by BA
+    }
+    UserData[] public users; //array of users
+    //uint index;
+    
+    constructor(address address_Adj) public {
+       // Set the creator of contract as the Authentication service (AuthServ)
+       AuthServ = msg.sender;
+       Adj = address_Adj; //setting the adjuducator contract address
+       //index = 0;
+    }
+    
+    modifier onlyAuthServ() {
+        require (msg.sender == AuthServ);
+        _; //execute remaining parts from the functions where this modifier is used after doing the above check 
+    }
+	
+	modifier onlyAdjAuth() {
+        require (msg.sender == Adj);
+        _; //execute remaining parts from the functions where this modifier is used after doing the above check 
+    }
+    
+    function registerUser(string memory _pid, string memory _pk, string memory _cert) onlyAuthServ public{
+        users.push(UserData(_pid, _pk, _cert));
+    }
+    
+    function deleteUser (string memory _pk) onlyAdjAuth public{
+		for (uint i=0; i<users.length; i++){
+			if (users[_index].pk == _pk){
+				delete users[_index].pid;
+				delete users[_index].pk;
+				delete users[_index].cert_pk;
+			}
+		}
+    }
+    
+}
